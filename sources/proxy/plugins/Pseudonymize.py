5a97f98e074bc094b1c31f162400e38d9ff32a7e
Tom Petersen <toomy1@gmx.de>
2017-11-16 15:15:12 +0100

    Pseudonym creation in external pseudonym service for pseudonym plugin

--- sources/proxy/plugins/Pseudonymize.py
+++ sources/proxy/plugins/Pseudonymize.py
@@ -1,7 +1,20 @@
+import requests
+
 from proxy.plugin import AbstractPlugin
 
 
 class Pseudonymize(AbstractPlugin):
 
+    SERVICE_URL = 'http://127.0.0.1:8000/store/pseudonym'
+    TIMEOUT = 1.0
+
     def handle_data(self, data: str, **kwargs):
-        return "PSEUDONYM(" + data + ")"
+        try:
+            request_data = {'content': data}
+            r = requests.post(self.SERVICE_URL, data=request_data, timeout=self.TIMEOUT)
+            r.raise_for_status()
+            result = r.json()
+            return result['pseudonym']
+        except Exception as e:
+            # TODO: what to do here?
+            raise
