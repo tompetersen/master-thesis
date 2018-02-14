43e6880165cc7a22c25b3d81f2065be644969ab2
Tom Petersen <toomy1@gmx.de>
2018-02-14 19:47:50 +0100

    Basic auth in pseudonymise plugin

--- sources/proxy/plugins/Pseudonymize.py
+++ sources/proxy/plugins/Pseudonymize.py
@@ -3,6 +3,7 @@ import datetime
 import requests
 import hashlib
 from nacl import utils, hash
+from requests.auth import HTTPBasicAuth
 
 from requests.exceptions import RequestException
 from requests.models import Response
@@ -15,11 +16,14 @@ class Pseudonymize(AbstractPlugin):
 
     SERVICE_URL = '127.0.0.1'
     SERVICE_PORT = 8000
+    USERNAME = 'store'
+    PASSWORD = 'test1234'
 
     def __init__(self):
         try:
+            self._apicaller = ServiceApiCaller(self.SERVICE_URL, self.SERVICE_PORT, self.USERNAME, self.PASSWORD)
             self.mac_key = None
-            self.pk, self.pseudonym_update_interval = ServiceApiCaller.get_config(self.SERVICE_URL, self.SERVICE_PORT)
+            self.pk, self.pseudonym_update_interval = self._apicaller.get_config()
             print('\tReceived service public key: ' + str(self.pk.g_a))
             print('\tReceived service pseudonym_update_interval: ' + str(self.pseudonym_update_interval))
         except Exception:
@@ -32,7 +36,7 @@ class Pseudonymize(AbstractPlugin):
 
         search_token = hash.blake2b(data.encode(), key=self.mac_key)
         encrypted_content = ThresholdCrypto.encrypt_message(data, self.pk).to_json()
-        pseudonym = ServiceApiCaller.get_pseudonym_for_data(self.SERVICE_URL, self.SERVICE_PORT, encrypted_content, search_token)
+        pseudonym = self._apicaller.get_pseudonym_for_data(encrypted_content, search_token)
 
         return pseudonym
 
@@ -58,35 +62,38 @@ class ServiceApiError(Exception):
 
 class ServiceApiCaller:
 
-    @staticmethod
-    def get_config(address: str, port: int) -> (PublicKey, int):
+    def __init__(self, address, port, username, password):
+        self._address = address
+        self._port = port
+        self._username = username
+        self._password = password
+
+    def get_config(self) -> (PublicKey, int):
         route = 'api/config/'
-        response = ServiceApiCaller._call_service_api(address, port, route)
+        response = self._call_service_api(route)
         response_dict = response.json()
         pk = response_dict['public_key']
         pseudonym_update_interval = response_dict['pseudonym_update_interval']
 
         return PublicKey.from_dict(pk), int(pseudonym_update_interval)
 
-    @staticmethod
-    def get_pseudonym_for_data(address: str, port: int, encrypted_content: str, search_token: str) -> str:
+    def get_pseudonym_for_data(self, encrypted_content: str, search_token: str) -> str:
         route = 'api/pseudonym/'
 
         request_data = {'content': encrypted_content, 'search_token': search_token}
-        response = ServiceApiCaller._call_service_api(address, port, route, request_data)
+        response = self._call_service_api(route, request_data)
         result = response.json()
 
         return result['pseudonym']
 
-    @staticmethod
-    def _call_service_api(address: str, port: int, route: str, data=None) -> Response:
+    def _call_service_api(self, route: str, data=None) -> Response:
         try:
-            url = 'http://' + address + ':' + str(port) + '/' + route
+            url = 'http://' + self._address + ':' + str(self._port) + '/' + route
 
             if data:
-                result = requests.post(url, data=data)
+                result = requests.post(url, data=data, auth=HTTPBasicAuth(self._username, self._password))
             else:
-                result = requests.get(url)
+                result = requests.get(url, auth=HTTPBasicAuth(self._username, self._password))
             result.raise_for_status()
 
             return result
